5a73e2b1569f74ff22e110fb374cf3751b578aea
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-04-19 17:05:16 -0300

    Integração com a API ViaCEP usando OpenFeign

---
+++ src/main/java/com/data_api/service/ViaCepService.java
@@ -0,0 +1,23 @@
+package com.data_api.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.data_api.model.Endereco;
+
+/**
+ * Client HTTP, criado via <b>OpenFeign</b>, para o consumo da API do
+ * <b>ViaCEP</b>.
+ * 
+ * @see <a href="https://spring.io/projects/spring-cloud-openfeign">Spring Cloud OpenFeign</a>
+ * @see <a href="https://viacep.com.br">ViaCEP</a>
+ * 
+ * @author rodrigobarr0s
+ */
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+}
